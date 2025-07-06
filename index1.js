284458e8a13246444f45a127b6a35647d79a6105
Vishnu Vardhan Singh <sugam.singh3@gmail.com>
2025-07-06 17:47:51 +0530

    Beggining

---
+++ index1.js
@@ -0,0 +1,47 @@
+let addbtn = document.querySelector('.addBtn')
+let delbtn = document.querySelector('.delBtn')
+let bar = document.getElementsByTagName('progress')
+let container3 = document.getElementsByClassName('container3')
+let container2 = document.querySelector(".container2")
+let max = document.getElementsByClassName('max')
+let min = document.getElementsByClassName('min')
+let radios = document.getElementsByClassName('task1 radio');
+
+let count = 0
+radios[0].checked = false
+console.log(radios[2].checked)
+
+addbtn.addEventListener('click', () => {
+
+    container2.append((container3[0].cloneNode(true)))
+    bar[0].max = container3.length
+    max[0].innerHTML = bar[0].max
+    min[0].innerHTML = bar[0].value
+})
+
+delbtn.addEventListener('click', () => {
+    if (container3.length > 1) {
+        container2.lastElementChild.remove()
+        // bar.value = container3.length
+        bar[0].max = container3.length
+        max[0].innerHTML = bar[0].max
+        min[0].innerHTML = bar[0].value
+    }
+})
+
+bar[0].max = container3.length
+bar[0].value = container3.length
+
+max[0].innerHTML = bar[0].max
+min[0].innerHTML = bar[0].value
+
+
+
+
+
+
+
+
+
+console.log(container2.lastElementChild)
+console.log(container3.length)
