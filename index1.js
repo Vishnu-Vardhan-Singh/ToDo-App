95514613f78d7f4af8e9ea72b016837b26386683
Vishnu Vardhan Singh <sugam.singh3@gmail.com>
2025-07-08 00:10:41 +0530

    Toggle radio button created

--- index1.js
+++ index1.js
@@ -6,11 +6,13 @@ let container2 = document.querySelector(".container2")
 let max = document.getElementsByClassName('max')
 let min = document.getElementsByClassName('min')
 let radios = document.getElementsByClassName('task1 radio');
-
 let count = 0
 
+// radios[0].checked =
 
 addbtn.addEventListener('click', () => {
+    document
+
 
     container2.append((container3[0].cloneNode(true)))
     bar[0].max = container3.length
@@ -29,11 +31,36 @@ delbtn.addEventListener('click', () => {
 })
 
 bar[0].max = container3.length
-bar[0].value = container3.length
 
 max[0].innerHTML = bar[0].max
 min[0].innerHTML = bar[0].value
 
 
-console.log(container2.lastElementChild)
-console.log(container3.length)
+let b = true;
+
+for (let i = 0; i < container3.length; i++) {
+    radios[i].addEventListener('click', function () {
+        if (b == true) {
+            radios[i].checked = b
+            b = false
+            console.log(radios[i].checked)
+        }
+        else {
+            radios[i].checked = b
+            b = true
+            console.log(radios[i].checked)
+        }
+
+        if (radios[i].checked == true) {
+            bar[i].value = i + 1
+        }
+        else {
+            bar[i].value = i
+        }
+    })
+    console.log(container3.length)
+}
+
+// console.log(container2.lastElementChild)
+// console.log(container3.length)
+
